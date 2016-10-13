8a727d83f0407c0cf108378e37e8751f146245f0
unknown <shane@valkry.com.au>
2016-10-13 19:33:06 +1100

    Implemented edit and update functionality

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+    @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
   end
   describe 'index' do
@@ -18,10 +18,10 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      post1 = FactoryGirl.build_stubbed(:post)
+      post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
-      expect(page).to have_content(/Post1|Post2/)
+      expect(page).to have_content(/Rationale|content/)
     end
   end
 
@@ -48,6 +48,27 @@ describe 'navigate' do
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("user Association")
+    end
+
+    describe 'edit' do
+      before do
+        @post = FactoryGirl.create(:post)
+      end
+      it 'can be reached by clicking edit on index page' do
+        visit posts_path
+
+        click_link("edit_#{@post.id}")
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be edited' do
+        visit edit_post_path(@post)
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[rationale]', with: "Edited content"
+        click_on "Save"
+
+        expect(page).to have_content("Edited content")
+      end
     end
   end
 end
