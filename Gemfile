47eb6e84333ea56f8f9fdeb81a4763026226b191
unknown <shane@valkry.com.au>
2016-10-14 10:46:42 +1100

    Integrated growl notifications for active record validations

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise', '~> 4.2'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'gritter', '~> 1.2'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      # @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
       @post = FactoryGirl.create(:post)
     end
     it 'can be created' do
